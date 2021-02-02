c0d01bdfe148cde97c9e5b8a715980f1098a235d
vasut02 <vasutiwari409@gmail.com>
2021-02-02 23:41:55 +0530

    copyFromClipBoard

--- client/src/components/play/Loading/Loading.js
+++ client/src/components/play/Loading/Loading.js
@@ -1,18 +1,37 @@
-import React from 'react'
-import logo from '../../navBar/logo.png';
+import React, { useRef, useState } from 'react'
 import './Loading.css'
 
 const Loading = ({ room_id }) => {
+
+    const [copySuccess, setCopySuccess] = useState('');
+
+    // your function to copy here
+
+    const copyToClipBoard = async e => {
+        try {
+            await navigator.clipboard.writeText(room_id);
+            setCopySuccess('Copied!');
+        } catch (err) {
+            try {
+                e.preventDefault();
+                e.clipboardData.setData('text/plain', room_id)
+                setCopySuccess('Copied!');
+            } catch (error) {
+                setCopySuccess('Failed to copy!');
+            }
+        }
+    };
+
     return (
         <div>
             Click Below to Copy Room Id and Share it with another player to join room
             <div id='loadingroom_idcontainer'>
-                <p
-                    onClick={() => navigator.clipboard.writeText(room_id)}
+                <p                    
+                    onClick={copyToClipBoard}
                     id='loadingroom_id'>
-                    {room_id}
+                    {room_id} 
                 </p>
-                
+                <p>{copySuccess}</p>
             </div>
         </div>
     )
